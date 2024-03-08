97008026461cc2ed803d03dc89c50bdd691b82f9
MohammadSA <mohammad.lenovotab@gmail.com>
2024-03-09 00:50:14 +0200

    Class

--- Backend/backend/gym/models.py
+++ Backend/backend/gym/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Class(models.Model):
+    name = models.CharField(max_length=20, unique=True, blank=False)
+    thumbnail = models.ImageField()
+    duration = models.IntegerField(max_length=3, blank=False)
+    intensity = models.CharField(max_length=10, blank=False)
+    level = models.CharField(max_length=10, blank=False)
+    schedule = []
+
+class Schedule(models.Model):
+    print("Schedule")
+
+class Trainer(models.Model):
+    print("Trainer")
+
+class Program(models.Model):
+    print("Program")
+
+class Article(models.Model):
+    print("Article")
+

1ed3e48a34d4e5cf6e900485cf143411580f6ca2
MohammdSA <mohammad.lenovotab@gmail.com>
2024-03-20 18:14:12 +0200

    Class Model

--- Backend/backend/gym/admin.py
+++ Backend/backend/gym/admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from .models import Class, Schedule, Trainer, Program, Article
 
 # Register your models here.
-admin.site.register(Class)
 admin.site.register(Schedule)
 admin.site.register(Trainer)
 admin.site.register(Program)
 admin.site.register(Article)
+
+class ClassAdmin(admin.ModelAdmin):
+    readonly_fields = ['date']
+
+
+admin.site.register(Class, ClassAdmin)
--- Backend/backend/gym/models.py
+++ Backend/backend/gym/models.py
@@ -3,11 +3,14 @@ from django.db import models
 # Create your models here.
 class Class(models.Model):
     name = models.CharField(max_length=20, unique=True, blank=False)
+    description = models.CharField(max_length=100, blank=False, null=True)
     thumbnail = models.ImageField()
     duration = models.IntegerField(blank=False)
-    intensity = models.CharField(max_length=10, blank=False)
-    level = models.CharField(max_length=10, blank=False)
-    schedule = models.TextField(blank=False, null=True)
+    intensity = models.CharField(max_length=20, blank=False)
+    level = models.CharField(max_length=20, blank=False)
+    schedule = models.JSONField()
+    date = models.DateTimeField(auto_now_add=True)
+    category = models.CharField(max_length=50, blank=False, null=True)
 
     def __str__(self):
         return f"{self.name} Class"

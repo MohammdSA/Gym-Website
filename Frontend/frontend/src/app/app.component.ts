fbe8e4182f4e5f5360b7eadb59d44c1d8899093e
MohammdSA <mohammad.lenovotab@gmail.com>
2024-04-02 04:52:14 +0300

    Footer

--- Frontend/frontend/src/app/app.component.ts
+++ Frontend/frontend/src/app/app.component.ts
@@ -40,14 +40,34 @@ import {HomeComponent} from './home/home.component';
 
           <footer>
             <div>
-              <div>
-                <img src="#" alt="footer-img">
-                <h2>Follow us on Instagram</h2>
+              <div class="footerBanner">
+                <img [src]="'assets/Follow%20Us%20on%20instagram.jpg'" alt="Follow Us on instagram">
+                <div class="footerBannerContent">
+                  <h2 class="followUsOnInstagram">Follow us on <a href="#">Instagram</a> </h2>
+                </div>
               </div>
-              <div>
-                <div></div>
-                <div></div>
-                <div></div>
+              <div class="footerContainer">
+                <div class="footerCol1">
+                  <div class="footerLogo">
+                    <img [src]="'assets/Logo.png'" alt="logo" />
+                    <h2>Strengthy</h2>
+                  </div>
+                  <p>It is a long established fact that a reader will be distracted by the readable.</p>
+                  <div class="footerContactUs">
+                    <div class="call">
+
+                    </div>
+                    <div class="mail">
+
+                    </div>
+                  </div>
+                </div>
+                <div class="footerCol2">
+
+                </div>
+                <div class="footerCol3">
+
+                </div>
               </div>
             </div>
             <div class="copyRight">Copyright © <Span style="color: var(--blue)">FitnessFit</Span> | Designed byVictorFlow Templates - Powered by Webflow</div>
